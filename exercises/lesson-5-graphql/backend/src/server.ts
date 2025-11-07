e7632c0c111881507f2d581cef188c40b2815b6d
ali-elhannouti-vinci <ali.elhannouti@student.vinci.be>
2025-11-07 12:30:43 +0100

    query test

--- exercises/lesson-5-graphql/backend/src/server.ts
+++ exercises/lesson-5-graphql/backend/src/server.ts
@@ -1,42 +1,57 @@
-import cors from 'cors';
-import express, { type Express } from 'express';
-import helmet from 'helmet';
-import { pino } from 'pino';
-import { healthCheckRouter } from '@/api/healthCheck/healthCheckRouter';
-import userRouter from '@/api/user/userRouter';
-import errorHandler from '@/common/middleware/errorHandler';
-import rateLimiter from '@/common/middleware/rateLimiter';
-import requestLogger from '@/common/middleware/requestLogger';
-import { env } from '@/common/utils/envConfig';
-import expenseRouter from './api/expense/expenseRouter';
-import transferRouter from './api/transfer/transferRouter';
-import transactionRouter from './api/transaction/transactionRouter';
+import cors from "cors";
+import express, { type Express } from "express";
+import helmet from "helmet";
+import { pino } from "pino";
+import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
+import userRouter from "@/api/user/userRouter";
+import errorHandler from "@/common/middleware/errorHandler";
+import rateLimiter from "@/common/middleware/rateLimiter";
+import requestLogger from "@/common/middleware/requestLogger";
+import { env } from "@/common/utils/envConfig";
+import expenseRouter from "./api/expense/expenseRouter";
+import transferRouter from "./api/transfer/transferRouter";
+import transactionRouter from "./api/transaction/transactionRouter";
+import graphqlMiddleware from "./graphql/middleware";
+import { ruruHTML } from "ruru/server";
 
-const logger = pino({ name: 'server start' });
+const logger = pino({ name: "server start" });
 const app: Express = express();
 
-// Set the application to trust the reverse proxy
-app.set('trust proxy', true);
+if (env.isDevelopment) {
+  const config = { endpoint: "/graphql" };
+  // Serve Ruru HTML
+  app.get("/ruru", (req, res) => {
+    res.format({
+      html: () => res.status(200).send(ruruHTML(config)),
+      default: () => res.status(406).send("Not Acceptable"),
+    });
+  });
 
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
-app.use(helmet());
-app.use(rateLimiter);
+  // Set the application to trust the reverse proxy
+  app.set("trust proxy", true);
 
-// Request logging
-app.use(requestLogger);
+  // Middlewares
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+  app.use(helmet());
+  app.use(rateLimiter);
 
-// Routes
-app.use('/health-check', healthCheckRouter);
+  // Request logging
+  app.use(requestLogger);
 
-app.use('/api/users', userRouter);
-app.use('/api/expenses', expenseRouter);
-app.use('/api/transfers', transferRouter);
-app.use('/api/transactions', transactionRouter);
+  // Routes
+  app.use("/health-check", healthCheckRouter);
 
-// Error handlers
-app.use(errorHandler());
+  app.use("/api/users", userRouter);
+  app.use("/api/expenses", expenseRouter);
+  app.use("/api/transfers", transferRouter);
+  app.use("/api/transactions", transactionRouter);
+
+  // Error handlers
+  app.use(errorHandler());
+
+  app.use("/graphql", graphqlMiddleware);
+}
 
 export { app, logger };
